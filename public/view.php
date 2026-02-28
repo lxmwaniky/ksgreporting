29b3b8b92e74f1ffb1ce2b2552b01a265cd54919
Langat18 <langatclement18@gmail.com>
2026-02-28 13:27:50 +0300

    Fix: Reduce report view logo and removal of © in report print

--- public/view.php
+++ public/view.php
@@ -27,6 +27,8 @@ if (!$report) {
     exit;
 }
 
+$bodyClass = 'hide-footer';
+
 require_once __DIR__ . '/../templates/header.php';
 require_once __DIR__ . '/../templates/report_view.php';
 require_once __DIR__ . '/../templates/footer.php';
